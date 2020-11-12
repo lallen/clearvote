393a544db3055abc6fd7efe4ce2fc601a9e6fc52
lallen <lallen@pop-os.localdomain>
2020-11-11 16:40:03 -0800

    edit templated

--- clearvote/urls.py
+++ clearvote/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from news.views import frontpage
+
 urlpatterns = [
+    path('', frontpage, name='frontpage'),
     path('admin/', admin.site.urls),
 ]
--- news/views.py
+++ news/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+from .models import New
+
 # Create your views here.
+def frontpage(request):
+    news = New.objects.all()
+    return render(request, 'blog/frontpage.html', {'news':news})
